80b80a786721b2b4fda7070b9f085edb033749d8
UtsavVanodiya7 <utsavgajjar5@gmail.com>
2021-03-21 13:59:21 -0400

    Added methods for supporting separate driver page requests.

--- WebApp/optimalPath.py
+++ WebApp/optimalPath.py
@@ -7,29 +7,29 @@ import geopy.distance
 from math import atan2, pi
 from sklearn.cluster import KMeans
 
+
 def build_graph(east, west, south, north):
     """
     Define the limits of the map by specifying the boundary longitudes
     and latitudes.
-    
+
     Create the graph object.
-    
+
     Return the graph.
     """
-    G = ox.graph_from_bbox(east, west, south, north, network_type = 'drive') 
+    G = ox.graph_from_bbox(east, west, south, north, network_type='drive')
     G = ox.add_edge_speeds(G)
     G = ox.add_edge_travel_times(G)
     return G
 
 
-
 def address_to_coord(address):
     """
-    Take address information and use Geopy to get its coordinates.   
+    Take address information and use Geopy to get its coordinates.
     """
-    locator = Nominatim(user_agent = 'capstone2_project')
+    locator = Nominatim(user_agent='capstone2_project')
     location = locator.geocode(address)
-    
+
     if location:
         return (location.latitude, location.longitude)
     else:
@@ -53,6 +53,7 @@ def cluster_destinations(destinations, drivers):
                 group.append(destinations[j])
         clusters.append(group)
     return clusters
+
 
 def coords_to_node(graph, coords):
     """
@@ -80,33 +81,36 @@ def dest_address_and_node(graph, dest_addresses):
         dest_dict[address] = node
     return dest_dict
 
+
 def dist(source_node, destination_node):
     """
-    Takes two nodes, gets their coordinates and uses Geopy to calculate 
-    the distance between them in metres. 
+    Takes two nodes, gets their coordinates and uses Geopy to calculate
+    the distance between them in metres.
     """
-    return geopy.distance.distance(node_to_coords(G, source_node), 
-                                    node_to_coords(G, destination_node)).m
+    return geopy.distance.distance(node_to_coords(G, source_node),
+                                   node_to_coords(G, destination_node)).m
 
-# We now work within a cluster 
-# We want to keep track of the destinations visited within that cluster 
+
+# We now work within a cluster
+# We want to keep track of the destinations visited within that cluster
 # and the optimal path from to visit each destination within the cluster.
 visited = []
 route = []
+
 
 def aStarMulti(start_node, destinations_nodes, graph, weight, visited, route):
     """
     This algorithm will determine the order in which to visit each destination in each cluster,
     and the optimal path from the start location, through all destinations.
-    We take the starting location, the list of destinations, the graph, the chosen weight - 
-    'travel_time' or 'length' - and the two previous lists. 
+    We take the starting location, the list of destinations, the graph, the chosen weight -
+    'travel_time' or 'length' - and the two previous lists.
     This is a recursive function.  While there are destinations still to visit, the reached
-    destination becomes the starting point for the next function call. 
+    destination becomes the starting point for the next function call.
     """
-   
+
     if not destinations_nodes:
-        return 'Done'    
-    # For each destination, calculate the distance to the starting point
+        return 'Done'
+        # For each destination, calculate the distance to the starting point
     distances = []
     for d in destinations_nodes:
         distances.append(dist(start_node, d))
@@ -122,11 +126,10 @@ def aStarMulti(start_node, destinations_nodes, graph, weight, visited, route):
     route.append(path)
     # Remove this destination from the list
     destinations_nodes.pop(minIndex)
-    # If there are still destinations to visit, call the function again with the 
+    # If there are still destinations to visit, call the function again with the
     # current location as the new starting point
     if destinations_nodes:
         aStarMulti(minNode, destinations_nodes, graph, weight, visited, route)
-
 
 
 def direction_between_points(pt1, pt2):
@@ -136,31 +139,31 @@ def direction_between_points(pt1, pt2):
     """
     delta_x = pt2[1] - pt1[1]
     delta_y = pt2[0] - pt1[0]
-    
-    degrees = atan2(delta_x, delta_y) / pi*180
-    
+
+    degrees = atan2(delta_x, delta_y) / pi * 180
+
     if degrees < 0:
         degrees = 360 + degrees
-    
-    compass = ['north', 'northeast', 'east', 'southeast', 
+
+    compass = ['north', 'northeast', 'east', 'southeast',
                'south', 'southwest', 'west', 'northwest', 'north']
-    
-    compass_dir = round(degrees/45)
+
+    compass_dir = round(degrees / 45)
     return compass[compass_dir]
 
 
 def full_route_info(graph, route, dests_list):
     """
     Creates a DataFrame containing the starting node, street name,
-    length, direction of travel, speed, and ending node, for each edge in the graph. 
-    We will also label which nodes represent the user's destinations. 
+    length, direction of travel, speed, and ending node, for each edge in the graph.
+    We will also label which nodes represent the user's destinations.
     """
     route_info = list()
     num_steps = len(route)
     i = 0
     j = 1
-# We want to get the information for every step in the route
-# We handle if any information is unlisted or unknown
+    # We want to get the information for every step in the route
+    # We handle if any information is unlisted or unknown
     while j < num_steps:
         step_info = list()
         step_info.append(route[i])
@@ -174,7 +177,7 @@ def full_route_info(graph, route, dests_list):
             # Sometimes street names are labeled as "links"
             # In these cases I will relabel them with the previous street
             try:
-                name = graph[route[i-1]][route[j-1]][0]['name']
+                name = graph[route[i - 1]][route[j - 1]][0]['name']
             except KeyError:
                 name = 'Unnamed'
         step_info.append(name)
@@ -186,10 +189,10 @@ def full_route_info(graph, route, dests_list):
         # We want to calculate the direction the edge is travelling in
         start = node_to_coords(graph, route[i])
         end = node_to_coords(graph, route[j])
-        # Use above function to calculate the direction from the start of the 
+        # Use above function to calculate the direction from the start of the
         # edge to its end
         step_info.append(direction_between_points(start, end))
-        
+
         seg_speed = graph[route[i]][route[j]][0]['speed_kph']
         step_info.append(seg_speed)
         step_info.append(route[j])
@@ -198,10 +201,10 @@ def full_route_info(graph, route, dests_list):
             step_info.append('Yes')
         else:
             step_info.append('No')
-        
+
         route_info.append(step_info)
-        i +=1
-        j +=1
+        i += 1
+        j += 1
 
     # Create a data frame of this information
     columns = ['Start Node', 'Street Name', 'Length', 'Direction', 'Speed', 'End Node', 'Destination?']
@@ -218,15 +221,15 @@ def get_turn_dir(graph, node1, node2, node3):
     p1 = [graph.nodes[node1]['x'], graph.nodes[node1]['y']]
     p2 = [graph.nodes[node2]['x'], graph.nodes[node2]['y']]
     p3 = [graph.nodes[node3]['x'], graph.nodes[node3]['y']]
-    
+
     # Next, define the line segments as vectors
-    p1top2 = [m - n for m,n in zip(p2,p1)]
-    p2top3 = [m - n for m,n in zip(p3,p2)]
-    p1top3 = [m - n for m,n in zip(p3,p1)]
-    
+    p1top2 = [m - n for m, n in zip(p2, p1)]
+    p2top3 = [m - n for m, n in zip(p3, p2)]
+    p1top3 = [m - n for m, n in zip(p3, p1)]
+
     # Next, compute the cross product of the vectors
     crossprod = np.cross(p1top3, p1top2)
-    
+
     if crossprod > 0:
         return 'Right'
     elif crossprod < 0:
@@ -239,7 +242,7 @@ def add_turns(route_df, graph):
     """
     Locate all the rows where the street name changes in the following row.
     Create a dictionary with these as keys - the values will calculate the direction
-    of the turn using the previous function. 
+    of the turn using the previous function.
     Adds a new column in the df showing the turn direction.
     """
     # Get the rows where the street changes in the following row
@@ -248,46 +251,48 @@ def add_turns(route_df, graph):
     for ind in route_df.index:
         if ind == route_df.index[-1]:
             pass
-        elif route_df.iloc[ind]['Street Name'] == route_df.iloc[ind+1]['Street Name'] and ind != route_df.index[-1]:
+        elif route_df.iloc[ind]['Street Name'] == route_df.iloc[ind + 1]['Street Name'] and ind != route_df.index[-1]:
             pass
         else:
             turn_indexes.append(ind)
-    # Use the above function to get the direction of these turns 
+    # Use the above function to get the direction of these turns
     turns_to_make = dict()
     for ind in turn_indexes:
-        turn_dir = get_turn_dir(graph, route_df.iloc[ind]['Start Node'], route_df.iloc[ind]['End Node'], route_df.iloc[ind+1]['End Node'])
+        turn_dir = get_turn_dir(graph, route_df.iloc[ind]['Start Node'], route_df.iloc[ind]['End Node'],
+                                route_df.iloc[ind + 1]['End Node'])
         turns_to_make[ind] = turn_dir
     # Create a new column in the dataframe for this information
     turns_list = [None] * len(route_df)
     for i in turns_to_make.keys():
         turns_list[i] = turns_to_make[i]
-    
+
     route_df['Turn Direction'] = turns_list
     return route_df
 
 
 def get_break_points(route_df):
     """
-    Get the row numbers where there are turns or destinations. 
+    Get the row numbers where there are turns or destinations.
     These rows will be kept in the route summary, and all rows in between each
     break point will be condensed into a single entry with the total length and
     appropriate starting and ending nodes.
     """
     break_points = []
     for row in range(len(route_df)):
-        if route_df['Destination?'][row]=='Yes' or route_df['Turn Direction'][row] != None:
+        if route_df['Destination?'][row] == 'Yes' or route_df['Turn Direction'][row] != None:
             break_points.append(row)
         row += 1
     return break_points
 
 
-
 segments = []
+
+
 def get_seg(start, segments, break_points, route_df):
     """
     This function will recursively create a list containing lists of the indexes for each leg.
     The identified breakpoints will be the starting points for each sublist.
-    
+
     """
     seg = []
     for row in range(start, len(route_df)):
@@ -296,10 +301,8 @@ def get_seg(start, segments, break_points, route_df):
             break
     segments.append(seg)
     current = seg[-1] + 1
-    if current <= len(route_df)-1:
+    if current <= len(route_df) - 1:
         get_seg(current, segments, break_points, route_df)
-        
-
 
 
 def get_simple_route(route_df, segments):
@@ -340,58 +343,57 @@ def get_simple_route(route_df, segments):
             leg_info['Turn Direction'] = route_df.loc[leg[-1]]['Turn Direction']
             simple_route.append(leg_info)
     return pd.DataFrame(simple_route)
-        
 
 
 def print_route_summary(simplified_route, dest_dict, graph):
     path = ""
-    visitOrder=[]
+    visitOrder = []
     """
     Display a simple summary with all relevant information for the driver.
     """
     # Get the destination nodes
-    destinations = simplified_route[simplified_route['Destination?']=='Yes']['End Node']
+    destinations = simplified_route[simplified_route['Destination?'] == 'Yes']['End Node']
     num_dest = len(destinations)
-    
-    path = path+('You have {} locations to vist:'.format(num_dest))
+
+    path = path + ('You have {} locations to visit:'.format(num_dest))
     # Using the dictionary, print off the destination addresses
     for d in destinations:
         for key, value in dest_dict.items():
             if value == d:
-                path = path + "\n"+(key)
+                path = path + "\n" + (key)
                 latLong = node_to_coords(graph, value)
                 mydict = {key: latLong}
                 visitOrder.append(mydict)
-    path = path+ "\n"+'\nStart!'
-    
+    path = path + "\n" + '\nStart!'
+
     for ind in simplified_route.index:
         # Display information for the leg of the route
         direct = simplified_route['Direction'][ind]
         name = simplified_route['Street Name'][ind]
         length = simplified_route['Length'][ind]
         speed = simplified_route['Speed'][ind]
-        path = path +"\n"+('Head {} on {} for {:.2f} metres at {} km/h.'.format(direct, name, length, speed))
-        # At the end of the leg, get the turn 
+        path = path + "\n" + ('Head {} on {} for {:.2f} metres at {} km/h.'.format(direct, name, length, speed))
+        # At the end of the leg, get the turn
         turn = simplified_route['Turn Direction'][ind]
-        # If there is no turn there are two cases - either it is marked "continue" - 
-        # in cases where a road changes speed or its name changes from west to east, for example - 
+        # If there is no turn there are two cases - either it is marked "continue" -
+        # in cases where a road changes speed or its name changes from west to east, for example -
         # or when we reach the last node on the route
         is_dest = simplified_route['Destination?'][ind]
         if turn == None and is_dest == 'No':
             # If we aren't at a destination, write continue
             if ind != simplified_route.index[-1]:
-                path = path + "\n"+('Continue')
+                path = path + "\n" + ('Continue')
             else:
-                # Display no turn information when the route is finished 
+                # Display no turn information when the route is finished
                 pass
         elif turn == None and is_dest == 'Yes':
             # Have arrived at a destination.
             destnode = simplified_route['End Node'][ind]
-            # Get the address 
+            # Get the address
             for key, value in dest_dict.items():
                 if value == destnode:
                     add = key
-            path = path +"\n"+('\nYou have reached a destination:' + add)
+            path = path + "\n" + ('\nYou have reached a destination:' + add)
             # This destination is unlikely to be at an actual graph node, so we want to provide some info
             # Get the coordinates of the node and the actual address
             nodecoords = node_to_coords(graph, destnode)
@@ -400,13 +402,15 @@ def print_route_summary(simplified_route, dest_dict, graph):
             dest_dist = geopy.distance.distance(nodecoords, addcoords).m
             # Calculate the direction in which the destination will be
             dest_dir = direction_between_points(nodecoords, addcoords)
-            path = path+ "\n"+('It is {:.2f} metres to your {}.\n'.format(dest_dist, dest_dir))
+            path = path + "\n" + ('It is {:.2f} metres to your {}.\n'.format(dest_dist, dest_dir))
         else:
             # If there is a turn, indicate it
-            path = path +"\n"+('Turn' + turn.upper())
-    
-    path = path +"\n"+('You have visited all of your destinations!')
-    return path,visitOrder
+            path = path + "\n" + ('Turn' + turn.upper())
+
+    path = path + "\n" + ('You have visited all of your destinations!')
+    return path, visitOrder
+
+
 def avoid_a_street(graph, street_list):
     """
     Will take a list of street names and return a new graph with these
@@ -433,10 +437,10 @@ def avoid_a_street(graph, street_list):
 
 def avoid_a_roadtype(graph, street_type_list):
     """
-    Will take a road type - 'motorway', 'secondary', 'tertiary', 'residential' - 
-    and remove this edges from the graph. 
+    Will take a road type - 'motorway', 'secondary', 'tertiary', 'residential' -
+    and remove this edges from the graph.
     This will allow a user to avoid high speed, or low speed, roads, if possible.
-    
+
     NOTE:
     This function will always follow avoid_a_street and so will take a copy of a graph.
     Graph copies have their information stored at inside a nested dictionary.
@@ -455,7 +459,6 @@ def avoid_a_roadtype(graph, street_type_list):
     return G2
 
 
-
 def get_starting_loc():
     """
     Prompts the user for their starting address.
@@ -464,7 +467,6 @@ def get_starting_loc():
     print('Ex: 23 Vanier Street Whitby')
     start = input()
     return start.title()
-
 
 
 def get_destinations():
@@ -504,7 +506,6 @@ def get_customizations():
     return avoid
 
 
-
 def get_customizations2():
     """
     Gets further user specifications based on road speeds.
@@ -518,8 +519,6 @@ def get_customizations2():
     return avoid
 
 
-
-
 def get_customizations3():
     """
     Gets further user specifications on what to optimize on.
@@ -529,7 +528,6 @@ def get_customizations3():
         return 'travel_time'
     else:
         return 'length'
-
 
 
 # Build the graph - this is Whitby/Oshawa for testing purposes - east, west, south, north
--- WebApp/speeder_api.py
+++ WebApp/speeder_api.py
@@ -3,6 +3,52 @@ import optimalPath
 
 app = Flask(__name__)
 
+latlngs = [
+    [43.90302, -78.94934],
+    [43.90391, -78.94969],
+    [43.90414, -78.94881],
+    [43.9044356, -78.9475582],
+    [43.9057112, -78.9426978],
+    [43.9059246, -78.9421041],
+    [43.906386, -78.9410528],
+    [43.9080293, -78.9398131],
+    [43.9093174, -78.9388471],
+    [43.91161, -78.93448],
+    [43.9152308, -78.9360707],
+    [43.91612, -78.93375],
+    [43.91722, -78.93224],
+    [43.91775, -78.92969],
+    [43.91894, -78.92914],
+    [43.9194318, -78.9271041],
+    [43.9206807, -78.9276331],
+    [43.9215542, -78.927781],
+    [43.92189, -78.92749],
+    [43.92214, -78.92655],
+    [43.9225035, -78.925025],
+    [43.9226064, -78.9244246],
+    [43.923342, -78.9212616],
+    [43.9239676, -78.9183999],
+    [43.9240111, -78.9182057],
+    [43.9240328, -78.9181111],
+    [43.9245833, -78.9156999],
+    [43.9247986, -78.9149332],
+    [43.924958, -78.9141492],
+    [43.926162, -78.9089493],
+    [43.9262651, -78.9083327],
+    [43.9262839, -78.9082408],
+    [43.9263722, -78.9082515],
+    [43.9267508, -78.9083716],
+    [43.9430467, -78.9155326],
+    [43.9436155, -78.9154965],
+    [43.9439194, -78.914629],
+    [43.9454576, -78.9076059],
+    [43.9457458, -78.9059392],
+    [43.9471678, -78.8995985],
+    [43.94626, -78.8992],
+    [43.94618, -78.89803],
+    [43.94607, -78.89592],
+    [43.9437411, -78.8949944]
+]
 
 
 @app.route('/')
@@ -11,36 +57,37 @@ def index():
     """
     return render_template('MapGUI.html')
 
+
 # Build the graph - this is Whitby/Oshawa for testing purposes - east, west, south, north
 G = optimalPath.build_graph(43.984503, 43.862879, -78.954552, -78.821660)
-def get_coordinates(locations, drivers):
+
+
+def get_coordinates(locations, drivers, streetsToAvoid, option):
     """
     This function should return dictionary as shown below.
     :param locations: list of addresses
     :param drivers: number of drivers
     :return:
     """
-    
-    #get the starting location and convert it to coordinates
+
+    # get the starting location and convert it to coordinates
     start = optimalPath.address_to_coord(locations[0])
-    
+
     # Get the coordinates for each destination
     dest_coords = []
-    
+
     for i in range(1, len(locations)):
         dest_coords.append(optimalPath.address_to_coord(locations[i]))
-    
-    
+
     # Store the destination addresses and corresponding graph nodes
     dest_dict = optimalPath.dest_address_and_node(G, locations[1:])
-    
-    
+
     # Cluster the destinations according to the number of drivers
     clusters = optimalPath.cluster_destinations(dest_coords, int(drivers))
-    
+
     # Convert locations into nodes
     start_node = optimalPath.coords_to_node(G, start)
-    
+
     # Get destination nodes
     destination_nodes = []
     for i in range(len(clusters)):
@@ -48,25 +95,24 @@ def get_coordinates(locations, drivers):
         for x in clusters[i]:
             inodes.append(optimalPath.coords_to_node(G, x))
         destination_nodes.append(inodes)
-    
-    
+
     dicts = []
-     # For each driver, get their route
+    # For each driver, get their route
     for driver in range(len(clusters)):
         coordinates = []
-        
+
         # Display the driver number
-        #print('\n\nDriver', driver+1, 'Route:')
-        # Store their destination nodes 
+        # print('\n\nDriver', driver+1, 'Route:')
+        # Store their destination nodes
         your_dests = destination_nodes[driver]
-        
+
         # Create empty list of their visited destinations and their overall route
         visited = []
         route = []
-        
+
         # Use the a* multi algorithm to build these lists
         optimalPath.aStarMulti(start_node, your_dests, G, 'travel_time', visited, route)
-        
+
         # Convert the list of lists into a single list
         route_list = []
         route_list.append(route[0][0])
@@ -74,19 +120,18 @@ def get_coordinates(locations, drivers):
             for j in i:
                 if route_list[-1] != j:
                     route_list.append(j)
-        
+
         # Check if there is a route
         if len(route_list) == 1:
             print('There is no possible route with these specification.')
             print('Please start over.')
             break
-        
-        
+
         # Get the full route information
         full_route_df = optimalPath.full_route_info(G, route_list, visited)
         # Add in the turns
         route_df = optimalPath.add_turns(full_route_df, G)
-        
+
         # Get the breakpoints in this driver's route
         break_points = optimalPath.get_break_points(route_df)
         # Split their route into segments
@@ -94,39 +139,43 @@ def get_coordinates(locations, drivers):
         optimalPath.get_seg(0, segments, break_points, route_df)
         # Simplify the route
         simplified_route = optimalPath.get_simple_route(route_df, segments)
-        
-        # Print the driver's route
-        summary,visitOrder = optimalPath.print_route_summary(simplified_route, dest_dict, G)
-        
-        #get coordinates for full path from start to finish
-        for node in simplified_route['Start Node']:
-            nCord = optimalPath.node_to_coords(G, node)
-            coordinates.append([nCord[0],nCord[1]])
-        
-        
-        #print(summary)
-        #print(visitOrder)
-        
-        #dst =[]
 
-       # for i in visited:
-         #   dst.append(int(i))
-        
+        # Print the driver's route
+        summary, visitOrder = optimalPath.print_route_summary(simplified_route, dest_dict, G)
+        summary = summary.replace("\n", "<br>")
+        summary = summary.replace("TurnLEFT", "Turn LEFT")
+        summary = summary.replace("TurnRIGHT", "Turn RIGHT")
+
+        # get coordinates for full path from start to finish
+        for node in route_df['Start Node']:
+            nCord = optimalPath.node_to_coords(G, node)
+            coordinates.append([nCord[0], nCord[1]])
+
+        # print(summary)
+        # print(visitOrder)
+
+        # dst =[]
+
+        # for i in visited:
+        #   dst.append(int(i))
+
+        print(route_df['Street Name'])
+
         info = {
             "startingPointName": route_df['Street Name'][0],
-            "destinations": visitOrder,
+            "endingPointName": list(visitOrder[-1].keys())[0],
+            "destinations": visitOrder[:-1],
             "coordinates": coordinates,
             "summary": summary,
-            }
-        
+        }
+
         dicts.append(info)
-     
-    #for i in range (len(dicts)):
-     #   print(i)
-      #  print(dicts[i])
-       # print("\n\n\n\n")
-     
-        
+
+    # for i in range (len(dicts)):
+    #   print(i)
+    #  print(dicts[i])
+    # print("\n\n\n\n")
+
     output = {
         "locations": dicts
     }
@@ -138,8 +187,35 @@ def get_coordinates(locations, drivers):
 def display_details():
     data = request.get_json()
     print(data)
-    output = get_coordinates(data['locations'], data['drivers'])
+    output = get_coordinates(data['locations'], data['drivers'], data['streetsToAvoid'],data['option'])
     return jsonify(output)
+
+
+@app.route("/driver")
+def driver():
+    return render_template("Driver.html")
+
+
+@app.route('/driver_details', methods=['POST'])
+def show_driver_detail():
+    """
+    This method should return route detail for specific driver.
+    Example can be seen below.
+    :return:
+    """
+    locations = {
+        "locations": [
+            {
+                "startingPointName": 'Vanier Street',
+                "endingPointName": "Durham College",
+                "destinations": [],
+                "coordinates": latlngs,
+                "summary": "Summary will be here...",
+            }
+        ]
+    }
+
+    return jsonify(locations)
 
 
 if __name__ == '__main__':
